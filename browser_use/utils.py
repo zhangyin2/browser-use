4fc6ce5e88ba5c401fc8bf7ebd3408c68439a7ef
magmueller <mamagnus00@gmail.com>
2024-11-07 13:41:23 +0100

    Cleaned up logger

--- browser_use/utils.py
+++ browser_use/utils.py
@@ -3,12 +3,26 @@ import time
 from functools import wraps
 from typing import Any, Callable, Coroutine, ParamSpec, TypeVar
 
+# Configure root logger to ERROR to suppress third-party logs
+logging.getLogger().setLevel(logging.ERROR)
+logging.getLogger('WDM').setLevel(logging.ERROR)  # WebDriver Manager
+logging.getLogger('httpx').setLevel(logging.ERROR)  # HTTP requests
+logging.getLogger('selenium').setLevel(logging.ERROR)  # Selenium
+logging.getLogger('urllib3').setLevel(logging.ERROR)  # URLLib
+logging.getLogger('asyncio').setLevel(logging.ERROR)  # Asyncio
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+
+handler = logging.StreamHandler()
+formatter = logging.Formatter('%(levelname)-8s %(message)s')
+handler.setFormatter(formatter)
+logger.addHandler(handler)
+
+
 # Define generic type variables for return type and parameters
 R = TypeVar('R')
 P = ParamSpec('P')
-
-logger = logging.getLogger(__name__)
-logging.basicConfig(level=logging.DEBUG)
 
 
 def time_execution_sync(additional_text: str = '') -> Callable[[Callable[P, R]], Callable[P, R]]:
@@ -18,7 +32,7 @@ def time_execution_sync(additional_text: str = '') -> Callable[[Callable[P, R]],
 			start_time = time.time()
 			result = func(*args, **kwargs)
 			execution_time = time.time() - start_time
-			logger.info(f'{additional_text} Execution time: {execution_time:.2f} seconds')
+			logger.debug(f'{additional_text} Execution time: {execution_time:.2f} seconds')
 			return result
 
 		return wrapper
@@ -35,7 +49,7 @@ def time_execution_async(
 			start_time = time.time()
 			result = await func(*args, **kwargs)
 			execution_time = time.time() - start_time
-			logger.info(f'{additional_text} Execution time: {execution_time:.2f} seconds')
+			logger.debug(f'{additional_text} Execution time: {execution_time:.2f} seconds')
 			return result
 
 		return wrapper
